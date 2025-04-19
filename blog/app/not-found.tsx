540bd3d5985b619d5ddfcd475fde1d8976055e4d
João Eduardo Panzo <joaoeduardopanzo@gmail.com>
2025-04-19 18:13:42 +0100

    chore: change content

--- blog/app/not-found.tsx
+++ blog/app/not-found.tsx
@@ -2,9 +2,9 @@ export default function NotFound() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
+        404 - Página não encontrada
       </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      <p className="mb-4">A página que você está procurando não existe</p>
     </section>
-  )
+  );
 }
